8b5bf09a655f3707eddb7b564c101f8a36646f73
Thibault Allançon <haltode@gmail.com>
2019-08-03 14:25:01 +0200

    fish: remove old config + update path (java, sbin)

--- fish/config.fish
+++ fish/config.fish
@@ -2,14 +2,18 @@
 # Export
 # -----------------------------------------------------------------------------
 
+set -gx PATH $PATH /usr/sbin
 set -gx PATH $PATH ~/scripts
-set -gx PATH $PATH ~/.local/bin
-set -gx PATH $PATH ~/.cargo/bin
 
 set -x TEXMFHOME ~/prologin/documents/texmf
 
+set -x JAVA_HOME /usr/lib/jvm/java-1.11.0-openjdk-amd64/
+set -gx PATH $PATH ~/tools/apache-maven-3.6.1/bin
+
 set -x EDITOR vim
 set -x TERMINAL termite
+# SSH hack to avoid "error opening terminal: xterm-termite"
+set -x TERM xterm
 set -x BROWSER firefox
 set -x PAGER less
 set -x MOVPLAY mpv
@@ -29,12 +33,6 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
-# Git
-alias gst='git status'
-# Fancy diff for git https://github.com/stevemao/diff-so-fancy
-alias gdf='git diff --color | diff_so_fancy | less -R'
-alias glg='git log --graph --pretty=format:\'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\' --abbrev-commit'
-
 # Config file
 alias cfg-i3='vim ~/dotfiles/i3/i3_config'
 alias cfg-vim='vim ~/dotfiles/vim/vimrc'
@@ -51,23 +49,3 @@ function T
     sleep 3
     transmission-remote-cli
 end
-
-# transfer.sh
-function transfer --description 'Upload a file to transfer.sh'
-    if [ $argv[1] ]
-        # write to output to tmpfile because of progress bar
-        set -l tmpfile ( mktemp -t transferXXX )
-        curl --progress-bar --upload-file $argv[1] https://transfer.sh/(basename $argv[1]) >> $tmpfile
-        cat $tmpfile
-        command rm -f $tmpfile
-    else
-        echo 'usage: transfer FILE_TO_TRANSFER'
-    end
-end
-
-# -----------------------------------------------------------------------------
-# Misc.
-# -----------------------------------------------------------------------------
-
-# OPAM configuration
-source /home/haltode/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
